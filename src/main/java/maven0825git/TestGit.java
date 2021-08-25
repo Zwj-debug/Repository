18a06d05321bf45096b229a8d61a4857903dff18
zwj <2974902969@qq.com>
2021-08-25 11:35:52 +0800

    分支

--- src/main/java/maven0825git/TestGit.java
+++ src/main/java/maven0825git/TestGit.java
@@ -5,5 +5,6 @@ public class TestGit {
 	public static void main(String[] args) {
 		System.err.println("git管理===");
 		System.err.println("git修改===");
+		System.err.println("git修改bug,xxx===");
 	}
 }
