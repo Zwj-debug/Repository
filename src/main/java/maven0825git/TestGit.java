1c4069a15b75eb393c9667c1b4154e063703d58b
zwj <2974902969@qq.com>
2021-08-25 16:27:05 +0800

    提交

--- src/main/java/maven0825git/TestGit.java
+++ src/main/java/maven0825git/TestGit.java
@@ -10,12 +10,9 @@ public class TestGit {
 		System.err.println("分支===");
 		System.err.println("主干开发===");
 		System.err.println("分支开发===");
-		
-
 		System.err.println("测试主干");
-
-		
 		System.out.println("测试分支");
+		System.out.println("测试git");
 
 	}
 }
